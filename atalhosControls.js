f84985e691a6104a62075aa28d0f2a3797528c4c
= <=>
2022-01-16 14:24:30 -0300

    ui.controls

---
+++ atalhosControls.js
@@ -0,0 +1,8 @@
+ui.controls.initialize({
+	control: "drawings",
+	tool: "freehand"
+});
+
+// Digitar "ui.controls" no console(F12) após selecionar a ferramenta
+// control: ui.controls.activeControl
+// tool: ui.controls.activeTool 

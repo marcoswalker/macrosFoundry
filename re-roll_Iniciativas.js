187d09432d2a4374f4daf80677f632d134a6a7f3
= <=>
2022-04-10 14:44:34 -0300

    Re-roll Iniciativas

--- re-roll_Iniciativas.js
+++ re-roll_Iniciativas.js
@@ -1,7 +1,6 @@
 await game.combats.active.resetAll();
 await game.combats.active.rollAll();
 while (game.combats.active.turn > 0) {
-    console.log(game.combats.active.turn);
     await game.combats.active.previousTurn();
 }
 
